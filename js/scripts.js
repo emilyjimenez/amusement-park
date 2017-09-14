64435efec6043d0f8c1ae7554ed22a129c685c88
Emily Wells Jimenez and Rane Fields <emily@>
2017-09-14 09:54:40 -0700

    add list of rides that displays based on height

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+  $("#height-form").submit(function(event) {
+    event.preventDefault();
+    var height = parseFloat($("#height-form input").val());
+    if (height <= 1) {
+      $(".for-adults").hide();
+      $(".for-kids").show();
+    } else {
+      $(".for-kids").hide();
+      $(".for-adults").show();
+    }
 
+    $("#ride-list").removeClass("hide");
+  });
+});
